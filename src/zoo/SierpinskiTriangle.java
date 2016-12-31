d6b82a4ce2879a0e5cc70a53698f50c55b82e9de
Loïc Fejoz <loic@fejoz.net>
2016-12-31 17:13:09 +0100

    Add Sierpinski example based on rule declaration.

---
+++ src/zoo/SierpinskiTriangle.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package zoo;
+
+import java.io.IOException;
+
+import shapegrammar.Node;
+import turtle.TextualTurtle;
+import turtle.Turtle;
+import turtle.TurtleInterpreter;
+
+/**
+ * @author loic
+ *
+ */
+public class SierpinskiTriangle {
+	
+	public static void main(final String[] args) throws IOException {
+		final TurtleInterpreter<Turtle> interp = new TurtleInterpreter<>();
+		interp.declareRule('A', "A−B+A+B−A", "F");
+		interp.declareRule('B', "BB", "F");
+		final Node<? super Turtle> axiom = interp.createAxiom("A-B-B");
+		interp.evaluate(axiom, 1);
+		axiom.render(new TextualTurtle());
+		for(int i = 0; i < 5; i++) {
+			System.out.println();
+			interp.evaluate();
+			axiom.render(new TextualTurtle());
+		}
+	}
+
+}
